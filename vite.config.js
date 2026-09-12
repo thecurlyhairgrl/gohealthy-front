7023caaab3bc5615071a28ef04b6623d8be69cd9
s-Dante <146044384+s-Dante@users.noreply.github.com>
2026-09-12 03:55:47 -0600

    fix: Asegurando el funcionamiento

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    server: {
+    host: true,       // ← esto es lo que le falta probablemente
+    port: 5173,
+  },
 })
